8ab883a26c47438984b7f431cf9f7deff6f4e6b2
Haz3L <haz3l@haz3l.duckdns.org>
2022-04-13 10:57:39 +0300

    Fix IconBadge for mobile

--- components/icon-badge.js
+++ components/icon-badge.js
@@ -1,4 +1,5 @@
 import { Tooltip, Box, useColorModeValue } from '@chakra-ui/react'
+import { useState } from 'react'
 
 export const CustomBadge = ({ icon, children }) => (
   <Box
@@ -26,9 +27,10 @@ export const IconBadge = ({
   borderColor
 }) => {
   const defaultBorder = useColorModeValue('gray.300', 'whiteAlpha.300')
+  const [isLabelOpen, setIsLabelOpen] = useState(false)
 
   return (
-    <Tooltip label={tooltip}>
+    <Tooltip label={tooltip} isOpen={isLabelOpen}>
       <Box
         p={2}
         border="1px solid"
@@ -40,6 +42,9 @@ export const IconBadge = ({
           typeof borderColor === 'undefined' ? defaultBorder : borderColor
         }
         _hover={{ transform: 'scale(1.1)' }}
+        onMouseEnter={() => setIsLabelOpen(true)}
+        onMouseLeave={() => setIsLabelOpen(false)}
+        onClick={() => setIsLabelOpen(true)}
       >
         {icon}
       </Box>
