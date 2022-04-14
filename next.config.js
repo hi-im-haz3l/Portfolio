c42988d2852513644ef79480179b884240b5120a
Haz3L <haz3l@haz3l.duckdns.org>
2022-04-15 00:33:05 +0300

    Include dead code cleaner

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const isAnalyze = typeof process.env.BUNDLE_ANALYZE !== 'undefined'
+const ThreeMinifierPlugin = require('@yushijinhun/three-minifier-webpack')
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
     locales: ['en-US', 'vi-VN', 'fi-FI'],
     defaultLocale: 'en-US'
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (isAnalyze) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: isServer ? 1111 : 1112,
+          openAnalyzer: true
+        })
+      )
+    }
+    if (!dev) {
+      config.cache = false
+      const threeMinifier = new ThreeMinifierPlugin()
+      config.plugins.unshift(threeMinifier)
+      config.resolve.plugins.unshift(threeMinifier.resolver)
+      config.experiments = { topLevelAwait: true };
+    }
+    return config
   }
 }
