65252778a30f6d4c73898e5b8bb5c9219cb4cf63
Haz3L <haz3l@haz3l.duckdns.org>
2022-04-17 12:06:41 +0300

    Add Horizontal Scroll

--- components/comforts.js
+++ components/comforts.js
@@ -1,34 +1,6 @@
-import {
-  Flex,
-  Box,
-  SimpleGrid,
-  HStack,
-  useColorModeValue
-} from '@chakra-ui/react'
-import { IconBadge } from './icon-badge'
-BsLightningChargeFill
-import { BsLightningChargeFill } from 'react-icons/bs'
-import { GiCubes } from 'react-icons/gi'
-import { RiReactjsLine } from 'react-icons/ri'
-import {
-  SiNextdotjs,
-  SiBootstrap,
-  SiPhp,
-  SiArduino,
-  SiEspressif,
-  SiC,
-  SiPopos,
-  SiNginx,
-  SiGnubash,
-  SiMariadb,
-  SiBlender,
-  SiAutodesk,
-  SiAdobephotoshop,
-  SiAdobeillustrator,
-  SiOpencv,
-  SiPython
-} from 'react-icons/si'
-import { useIntl } from 'react-intl'
+import { Flex, Box, SimpleGrid, useColorModeValue } from '@chakra-ui/react'
+import ComfortsCore from './comforts-core'
+import HScroll from './horizontal-scroll'
 
 const Comfort = ({ children, ...props }) => (
   <Flex
@@ -50,222 +22,46 @@ const Comfort = ({ children, ...props }) => (
 
 const Heading = ({ children }) => <Box w={[200, 150, 200]}>{children}</Box>
 
-const Technologies = ({ children }) => (
-  <HStack
-    w={[200, 150, 200]}
-    justifyContent={['flex-start', 'flex-end', 'flex-start']}
-    pt={[2, 0, 2]}
-  >
-    {children}
-  </HStack>
-)
+const Switch = ({ base }) => {
+  const Core = ComfortsCore()
+  const segment = []
+  if (base == 't') {
+    for (let o = 0; o < 2; o++) {
+      let temp = o * 3
+      segment.push(
+        Core.slice(temp, temp + 3).map(({ title, icons }, i) => (
+          <Comfort key={i} mb={3}>
+            <Heading>{title}</Heading>
+            {icons}
+          </Comfort>
+        ))
+      )
+    }
+  }
 
-const Comforts = () => {
-  const { formatMessage: t } = useIntl()
-  const CTooltip =
-    'C (' +
-    t({
-      id: 'Home.Comfortable.Content.Embedded.C',
-      defaultMessage: 'Undefined'
-    }) +
-    ')'
-
-  return (
-    <SimpleGrid columns={[1, 1, 2]} spacing={3}>
-      <Comfort>
-        <Heading>
-          {t({
-            id: 'Home.Comfortable.Content.Web',
-            defaultMessage: 'Undefined'
-          })}
-        </Heading>
-        <Technologies>
-          <IconBadge
-            icon={<SiNextdotjs />}
-            color={useColorModeValue('#000', undefined)}
-            bg={useColorModeValue(undefined, '#000')}
-            tooltip="Next.js"
-          />
-          <IconBadge
-            icon={<SiPhp />}
-            color={useColorModeValue('#4b568b', undefined)}
-            bg={useColorModeValue(undefined, '#5d6aab')}
-            borderColor={useColorModeValue('#4b568b', undefined)}
-            tooltip="PHP"
-          />
-          <IconBadge
-            icon={<SiBootstrap />}
-            color={useColorModeValue('#8f62d2', undefined)}
-            bg={useColorModeValue(undefined, '#7e4eca')}
-            borderColor={useColorModeValue('#8f62d2', undefined)}
-            tooltip="Bootstrap"
-          />
-          <IconBadge
-            icon={<RiReactjsLine />}
-            color={useColorModeValue('#00c2e5', undefined)}
-            bg={useColorModeValue(undefined, '#00aec5')}
-            borderColor={useColorModeValue('#00c2e5', '#007989')}
-            tooltip="React"
-          />
-          <IconBadge
-            icon={<BsLightningChargeFill />}
-            color={useColorModeValue('#4cc8c3', undefined)}
-            bg={useColorModeValue(undefined, '#5bc0c1')}
-            borderColor={useColorModeValue('#4cc8c3', '#408889')}
-            tooltip="Chakra UI"
-          />
-        </Technologies>
-      </Comfort>
-      <Comfort>
-        <Heading>
-          {t({
-            id: 'Home.Comfortable.Content.Linux',
-            defaultMessage: 'Undefined'
-          })}
-        </Heading>
-        <Technologies>
-          <IconBadge
-            icon={<SiGnubash />}
-            color={useColorModeValue('#313134', undefined)}
-            bg={useColorModeValue(undefined, '#313134')}
-            borderColor={useColorModeValue('#013143', undefined)}
-            tooltip="Powerlevel10k"
-          />
-          <IconBadge
-            icon={<SiMariadb />}
-            color={useColorModeValue('#013143', undefined)}
-            bg={useColorModeValue(undefined, '#013143')}
-            borderColor={useColorModeValue('#013143', undefined)}
-            tooltip="MariaDB"
-          />
-          <IconBadge
-            icon={<SiPopos />}
-            color={useColorModeValue('#2c7078', undefined)}
-            bg={useColorModeValue(undefined, '#368b95')}
-            borderColor={useColorModeValue('#2c7078', undefined)}
-            tooltip="Pop!_OS"
-          />
-          <IconBadge
-            icon={<SiNginx />}
-            color={useColorModeValue('#0c8f49', undefined)}
-            bg={useColorModeValue(undefined, '#097039')}
-            borderColor={useColorModeValue('#097039', undefined)}
-            tooltip="Nginx"
-          />
-        </Technologies>
-      </Comfort>
-      <Comfort>
-        <Heading>
-          {t({
-            id: 'Home.Comfortable.Content.Embedded',
-            defaultMessage: 'Undefined'
-          })}
-        </Heading>
-        <Technologies>
-          <IconBadge
-            icon={<SiEspressif />}
-            color={useColorModeValue('#cd292c', undefined)}
-            bg={useColorModeValue(undefined, '#ae1614')}
-            borderColor={useColorModeValue('#cd292c', undefined)}
-            tooltip="ESP-IDF"
-          />
-          <IconBadge
-            icon={<SiC />}
-            color={useColorModeValue('#014b95', undefined)}
-            bg={useColorModeValue(undefined, '#00589d')}
-            borderColor={useColorModeValue('#014b95', undefined)}
-            tooltip={CTooltip}
-          />
-          <IconBadge
-            icon={<SiArduino />}
-            color={useColorModeValue('#008184', undefined)}
-            bg={useColorModeValue(undefined, '#00979d')}
-            borderColor={useColorModeValue('#008184', undefined)}
-            tooltip="Arduino"
-          />
-        </Technologies>
-      </Comfort>
-      <Comfort>
-        <Heading>
-          {t({
-            id: 'Home.Comfortable.Content.3D',
-            defaultMessage: 'Undefined'
-          })}
-        </Heading>
-        <Technologies>
-          <IconBadge
-            icon={<SiBlender />}
-            color={useColorModeValue('#e17910', undefined)}
-            bg={useColorModeValue(undefined, '#ef6302')}
-            borderColor={useColorModeValue('#e17910', '#933d00')}
-            tooltip="Blender"
-          />
-          <IconBadge
-            icon={<SiAutodesk />}
-            color={useColorModeValue('#d38901', undefined)}
-            bg={useColorModeValue(undefined, '#e19200')}
-            borderColor={useColorModeValue('#d38901', '#7e5200')}
-            tooltip="Fusion 360"
-          />
-          <IconBadge
-            icon={<GiCubes />}
-            color={useColorModeValue('#3b3b7a', undefined)}
-            bg={useColorModeValue(undefined, '#4b4b9b')}
-            borderColor={useColorModeValue('#3b3b7a', undefined)}
-            tooltip="MagicaVoxel"
-          />
-        </Technologies>
-      </Comfort>
-      <Comfort>
-        <Heading>
-          {t({
-            id: 'Home.Comfortable.Content.Vision',
-            defaultMessage: 'Undefined'
-          })}
-        </Heading>
-        <Technologies>
-          <IconBadge
-            icon={<SiOpencv />}
-            color={useColorModeValue('#013143', undefined)}
-            bg={useColorModeValue(undefined, '#313134')}
-            borderColor={useColorModeValue('#013143', undefined)}
-            tooltip="OpenCV"
-          />
-          <IconBadge
-            icon={<SiPython />}
-            color={useColorModeValue('#014b95', undefined)}
-            bg={useColorModeValue(undefined, '#0e5e99')}
-            borderColor={useColorModeValue('#014b95', undefined)}
-            tooltip="Python"
-          />
-        </Technologies>
-      </Comfort>
-      <Comfort>
-        <Heading>
-          {t({
-            id: 'Home.Comfortable.Content.Images',
-            defaultMessage: 'Undefined'
-          })}
-        </Heading>
-        <Technologies>
-          <IconBadge
-            icon={<SiAdobeillustrator />}
-            color={useColorModeValue('#833a00', '#300000')}
-            bg={useColorModeValue(undefined, '#df9713')}
-            borderColor={useColorModeValue('#d58101', '#7e5200')}
-            tooltip="Adobe Illustrator"
-          />
-          <IconBadge
-            icon={<SiAdobephotoshop />}
-            color={useColorModeValue('#00566e', '#00242e')}
-            bg={useColorModeValue(undefined, '#01afd3')}
-            borderColor={useColorModeValue('#01afd3', '#006276')}
-            tooltip="Adobe Photoshop"
-          />
-        </Technologies>
-      </Comfort>
+  return base == 't' ? (
+    <HScroll display={{ base: 'block', sm: 'none' }} tabs={segment} />
+  ) : (
+    <SimpleGrid
+      display={{ base: 'none', sm: 'grid' }}
+      columns={[1, 1, 2]}
+      spacing={3}
+    >
+      {Core.map(({ title, icons }, i) => (
+        <Comfort key={i}>
+          <Heading>{title}</Heading>
+          {icons}
+        </Comfort>
+      ))}
     </SimpleGrid>
   )
 }
+
+const Comforts = () => (
+  <Box>
+    <Switch base="t" />
+    <Switch base="f" />
+  </Box>
+)
 
 export default Comforts
---
+++ components/horizontal-scroll.js
@@ -0,0 +1,111 @@
+import { useState } from 'react'
+import { Box, Flex } from '@chakra-ui/react'
+import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion'
+
+const variants = {
+  enter: function (direction) {
+    return {
+      x: direction > 0 ? 700 : -700,
+      opacity: 0
+    }
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: function (direction) {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 700 : -700,
+      opacity: 0
+    }
+  }
+}
+
+const swipeConfidenceThreshold = 10000
+
+const HScroll = ({ display, tabs }) => {
+  const [[page, direction], setPage] = useState([0, 0])
+
+  return (
+    <Box display={display}>
+      <AnimateSharedLayout>
+        <Box w="59px" mt="-1.7em" ml="auto" pb={6}>
+          <Flex mt={2} justifyContent="space-between" position="relative">
+            {tabs.map((e, i) => {
+              const isActive = i === page
+              return (
+                <Box key={i} w="100%">
+                  {isActive && (
+                    <Box
+                      as={motion.div}
+                      w="100%"
+                      h="0.25em"
+                      borderRadius="full"
+                      bg="#ededee"
+                      position="relative"
+                      zIndex={1}
+                      layoutId="underline"
+                    />
+                  )}
+                </Box>
+              )
+            })}
+            <Box
+              w="100%"
+              h="0.25em"
+              borderRadius="full"
+              bg="#525252"
+              position="absolute"
+            />
+          </Flex>
+        </Box>
+        <Box h="18.4em" overflow="hidden" position="relative">
+          <AnimatePresence initial={false} custom={direction}>
+            <Box
+              w="100%"
+              position="absolute"
+              left="50%"
+              transform="translate(-50%, 0)"
+            >
+              <motion.section
+                key={page}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  x: {
+                    type: 'spring',
+                    stiffness: 300,
+                    damping: 30,
+                    duration: 2
+                  },
+                  opacity: { duration: 0.5 }
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                  const swipe = Math.abs(offset.x) * velocity.x
+
+                  if (swipe < -swipeConfidenceThreshold) {
+                    if (page < 1) setPage([page + 1, 1])
+                  } else if (swipe > swipeConfidenceThreshold) {
+                    if (page > 0) setPage([page - 1, -1])
+                  }
+                }}
+              >
+                {tabs[page]}
+              </motion.section>
+            </Box>
+          </AnimatePresence>
+        </Box>
+      </AnimateSharedLayout>
+    </Box>
+  )
+}
+
+export default HScroll
