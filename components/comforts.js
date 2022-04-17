3ccfc5f2a297653b36e74a845a81c186f58efdde
Haz3L <haz3l@haz3l.duckdns.org>
2022-04-18 00:43:25 +0300

    Remove obsolete components

--- components/comforts.js
+++ components/comforts.js
@@ -1,7 +1,7 @@
 import { Flex, Box, SimpleGrid, useColorModeValue } from '@chakra-ui/react'
 import ComfortsCore from './comforts-core'
 import HScroll from './horizontal-scroll'
-import { useState, useCallback, useEffect } from 'react'
+import WidthQuery from './width-query'
 
 const Comfort = ({ children, ...props }) => (
   <Flex
@@ -24,15 +24,15 @@ const Comfort = ({ children, ...props }) => (
 const Heading = ({ children }) => <Box w={[200, 150, 200]}>{children}</Box>
 
 const Switch = ({ base }) => {
-  const Core = ComfortsCore()
+  const core = ComfortsCore()
   const segment = []
   const PagesCount = 2
 
-  if (base == 't') {
+  if (base) {
     for (let o = 0; o < PagesCount; o++) {
       let temp = o * 3
       segment.push(
-        Core.slice(temp, temp + 3).map(({ title, icons }, i) => (
+        core.slice(temp, temp + 3).map(({ title, icons }, i) => (
           <Comfort key={i} mb={3}>
             <Heading>{title}</Heading>
             {icons}
@@ -42,19 +42,11 @@ const Switch = ({ base }) => {
     }
   }
 
-  return base === 't' ? (
-    <HScroll
-      display={{ base: 'block', sm: 'none' }}
-      tabs={segment}
-      PagesIndexes={PagesCount - 1}
-    />
+  return base ? (
+    <HScroll tabs={segment} PagesIndexes={PagesCount - 1} />
   ) : (
-    <SimpleGrid
-      display={{ base: 'none', sm: 'grid' }}
-      columns={[1, 1, 2]}
-      spacing={3}
-    >
-      {Core.map(({ title, icons }, i) => (
+    <SimpleGrid columns={[1, 1, 2]} spacing={3}>
+      {core.map(({ title, icons }, i) => (
         <Comfort key={i}>
           <Heading>{title}</Heading>
           {icons}
@@ -64,31 +56,6 @@ const Switch = ({ base }) => {
   )
 }
 
-const useMediaQuery = width => {
-  const [targetReached, setTargetReached] = useState(false)
-
-  const updateTarget = useCallback(e => {
-    if (e.matches) {
-      setTargetReached(true)
-    } else {
-      setTargetReached(false)
-    }
-  }, [])
-
-  useEffect(() => {
-    const media = window.matchMedia(`(max-width: ${width}px)`)
-    media.addEventListener('change', updateTarget)
-
-    if (media.matches) {
-      setTargetReached(true)
-    }
-
-    return () => media.removeEventListener('change', updateTarget)
-  }, [])
-
-  return targetReached
-}
-
-const Comforts = () => <Switch base={useMediaQuery(480) ? 't' : 'f'} />
+const Comforts = () => <Switch base={WidthQuery(480) ? 'y' : undefined} />
 
 export default Comforts
--- components/horizontal-scroll.js
+++ components/horizontal-scroll.js
@@ -54,93 +54,91 @@ const SideIndicator = ({ direction, children }) => (
 
 const ConfidenceThreshold = 10000
 
-const HScroll = ({ display, tabs, PagesIndexes }) => {
+const HScroll = ({ tabs, PagesIndexes }) => {
   const [[page, direction], setPage] = useState([0, 0])
   const thumb = useColorModeValue('#000', '#fff')
 
   return (
-    <Box display={display}>
-      <AnimateSharedLayout>
-        <Box w="59px" mt="-1.7em" ml="auto" pb={6}>
-          <Flex mt={2} justifyContent="space-between" position="relative">
-            {tabs.map((e, i) => {
-              const isActive = i === page
-              return (
-                <Box key={i} w="100%">
-                  {isActive && (
-                    <Box
-                      as={motion.div}
-                      w="100%"
-                      h="0.25em"
-                      borderRadius="full"
-                      bg={thumb}
-                      position="relative"
-                      zIndex={1}
-                      layoutId="underline"
-                    />
-                  )}
-                </Box>
-              )
-            })}
-            <Box
-              w="100%"
-              h="0.25em"
-              borderRadius="full"
-              bg={useColorModeValue('#d1d1d1', '#525252')}
-              position="absolute"
-            />
-          </Flex>
-        </Box>
-        <Box h="18.4em" overflow="hidden" position="relative">
-          <SideIndicator direction="right">
-            {page < PagesIndexes ? <ChevronRightIcon /> : undefined}
-          </SideIndicator>
-          <SideIndicator direction="left">
-            {page > 0 ? <ChevronLeftIcon /> : undefined}
-          </SideIndicator>
-          <AnimatePresence initial={false} custom={direction}>
-            <Box
-              w="100%"
-              position="absolute"
-              left="50%"
-              transform="translate(-50%, 0)"
-            >
-              <motion.section
-                key={page}
-                custom={direction}
-                variants={variants}
-                initial="enter"
-                animate="center"
-                exit="exit"
-                transition={{
-                  x: {
-                    type: 'spring',
-                    stiffness: 300,
-                    damping: 30,
-                    duration: 2
-                  },
-                  opacity: { duration: 0.5 }
-                }}
-                drag="x"
-                dragConstraints={{ left: 0, right: 0 }}
-                dragElastic={1}
-                onDragEnd={(e, { offset, velocity }) => {
-                  const swipe = Math.abs(offset.x) * velocity.x
+    <AnimateSharedLayout>
+      <Box w="59px" mt="-1.7em" ml="auto" pb={6}>
+        <Flex mt={2} justifyContent="space-between" position="relative">
+          {tabs.map((e, i) => {
+            const isActive = i === page
+            return (
+              <Box key={i} w="100%">
+                {isActive && (
+                  <Box
+                    as={motion.div}
+                    w="100%"
+                    h="0.25em"
+                    borderRadius="full"
+                    bg={thumb}
+                    position="relative"
+                    zIndex={1}
+                    layoutId="underline"
+                  />
+                )}
+              </Box>
+            )
+          })}
+          <Box
+            w="100%"
+            h="0.25em"
+            borderRadius="full"
+            bg={useColorModeValue('#d1d1d1', '#525252')}
+            position="absolute"
+          />
+        </Flex>
+      </Box>
+      <Box h="18.4em" overflow="hidden" position="relative">
+        <SideIndicator direction="right">
+          {page < PagesIndexes ? <ChevronRightIcon /> : undefined}
+        </SideIndicator>
+        <SideIndicator direction="left">
+          {page > 0 ? <ChevronLeftIcon /> : undefined}
+        </SideIndicator>
+        <AnimatePresence initial={false} custom={direction}>
+          <Box
+            w="100%"
+            position="absolute"
+            left="50%"
+            transform="translate(-50%, 0)"
+          >
+            <motion.section
+              key={page}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: {
+                  type: 'spring',
+                  stiffness: 300,
+                  damping: 30,
+                  duration: 2
+                },
+                opacity: { duration: 0.5 }
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = Math.abs(offset.x) * velocity.x
 
-                  if (swipe < -ConfidenceThreshold) {
-                    if (page < PagesIndexes) setPage([page + 1, 1])
-                  } else if (swipe > ConfidenceThreshold) {
-                    if (page > 0) setPage([page - 1, -1])
-                  }
-                }}
-              >
-                {tabs[page]}
-              </motion.section>
-            </Box>
-          </AnimatePresence>
-        </Box>
-      </AnimateSharedLayout>
-    </Box>
+                if (swipe < -ConfidenceThreshold) {
+                  if (page < PagesIndexes) setPage([page + 1, 1])
+                } else if (swipe > ConfidenceThreshold) {
+                  if (page > 0) setPage([page - 1, -1])
+                }
+              }}
+            >
+              {tabs[page]}
+            </motion.section>
+          </Box>
+        </AnimatePresence>
+      </Box>
+    </AnimateSharedLayout>
   )
 }
 
