41f0cc127c55ac3c30fd19b633a0fe61a90e8eeb
Haz3L <haz3l@haz3l.duckdns.org>
2022-04-19 14:24:26 +0300

    Remove compat component

--- components/width-query.js
+++ components/width-query.js
@@ -1,6 +1,6 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 
-export const WidthLowerThan = width => {
+const WidthLowerThan = width => {
   const hasWindow = typeof window !== 'undefined'
   const getWindowDimensions = () => {
     if (hasWindow) return window.innerWidth <= width ? true : false
@@ -22,20 +22,4 @@ export const WidthLowerThan = width => {
   return windowDimensions
 }
 
-export const WidthLowerThanCompat = width => {
-  const [windowDimensions, setWindowDimensions] = useState(false)
-  const updateTarget = useCallback(e => {
-    if (e.matches) setWindowDimensions(true)
-    else setWindowDimensions(false)
-  }, [])
-
-  useEffect(() => {
-    const media = window.matchMedia(`(max-width: ${width}px)`)
-    media.addEventListener('change', updateTarget)
-    if (media.matches) setWindowDimensions(true)
-
-    return () => media.removeEventListener('change', updateTarget)
-  }, [])
-
-  return windowDimensions
-}
+export default WidthLowerThan
