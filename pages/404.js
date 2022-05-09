ff17fcc5d0d4d8ed43f981dbc259ef95357aba9e
Haz3L <haz3l@haz3l.duckdns.org>
2022-05-09 12:25:48 +0300

    Add Layout

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,17 @@
 import { Box, Heading, Text, Container } from '@chakra-ui/react'
 import { RiPantoneLine } from 'react-icons/ri'
 import { useIntl } from 'react-intl'
+import Layout from '../components/layouts/article'
 import SlidingButton from '../components/slide-button'
 
 const NotFound = () => {
   const { formatMessage: t } = useIntl()
 
   return (
+    <Layout title={t({ id: '404.Title', defaultMessage: 'Undefined' })}>
     <Container>
       <Heading as="h1">
-        {t({ id: '404.Title', defaultMessage: 'Undefined' })}
+        {t({ id: '404.Header', defaultMessage: 'Undefined' })}
       </Heading>
       <Text>{t({ id: '404.Description', defaultMessage: 'Undefined' })}</Text>
       <Box my={6} align="center">
@@ -18,6 +20,7 @@ const NotFound = () => {
         </SlidingButton>
       </Box>
     </Container>
+    </Layout>
   )
 }
 
