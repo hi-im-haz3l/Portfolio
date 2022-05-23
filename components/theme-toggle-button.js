610812d9eac65d795d220d5cb0a8fbd4bb7741dc
Haz3L <haz3l@haz3l.duckdns.org>
2022-05-23 19:47:41 +0300

    Animate ThemeToggleButton

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from 'framer-motion'
-import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { Box, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+import { RiPantoneLine } from 'react-icons/ri'
 
 const ThemeToggleButton = ({ ariaLabel }) => {
   const { toggleColorMode } = useColorMode()
@@ -15,14 +16,50 @@ const ThemeToggleButton = ({ ariaLabel }) => {
         exit={{ y: 20, opacity: 0 }}
         transition={{ duration: 0.2 }}
       >
-        <IconButton
-          aria-label={ariaLabel}
-          colorScheme={useColorModeValue('purple', 'orange')}
-          icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-          onClick={toggleColorMode}
-          boxShadow="lg"
+        <Box
+          p={2}
+          h="2.5em"
+          w="2.5em"
+          as="button"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
           borderRadius="lg"
-        />
+          boxShadow="lg"
+          aria-label={ariaLabel}
+          bg={useColorModeValue('#805ad5', '#fbd38d')}
+          color={useColorModeValue('white', 'black')}
+          onClick={toggleColorMode}
+          _hover={{
+            '&>span': {
+              transform: 'translateX(-.2em) translateY(.25em)',
+              opacity: '1'
+            },
+            '&>div': {
+              transform: 'translateX(.7em) translateY(-.8em)',
+              fontSize: '10'
+            }
+          }}
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            position="absolute"
+            transition="350ms"
+          >
+            {useColorModeValue(<MoonIcon />, <SunIcon />)}
+          </Box>
+          <Box
+            as="span"
+            position="absolute"
+            transform="translateX(-1em) translateY(.25em)"
+            opacity="0"
+            transition="350ms"
+            fontSize={18}
+          >
+            <RiPantoneLine />
+          </Box>
+        </Box>
       </motion.div>
     </AnimatePresence>
   )
